d73e5c8ff0fb1deb32414dfcdb44e844694d69a7
Wilhelm Willie <wilhelmwillie@gmail.com>
2014-12-22 18:39:51 -0500

    Renamed to_recepient to to_recipient

--- cardcreator/models.py
+++ cardcreator/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Card(models.Model):
-  to_recepient = models.CharField(max_length=36) # Name of the recepient
+  to_recipient = models.CharField(max_length=36) # Name of the recipient
   from_sender = models.CharField(max_length=36) # Name of the sender
   message = models.CharField(max_length=560) # The message of the card
   img = models.CharField(max_length=36) # Stores image file name without extension

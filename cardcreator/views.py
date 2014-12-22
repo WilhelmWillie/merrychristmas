a53871b05b4a1937b89f8c7475f0d73f0d5961ae
Wilhelm Willie <wilhelmwillie@gmail.com>
2014-12-22 18:14:26 -0500

    Working on card creation form

--- cardcreator/views.py
+++ cardcreator/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import Http404
 
 from cardcreator.models import Card
 
@@ -10,4 +11,9 @@ def card(request, card_slug):
   return render(request, 'card.html', {'card': c})
 
 def create_card(request):
+  if request.method == 'POST':
+    print "Cash"
+  else:
+    raise Http404
+
   return render(request, 'home.html')
